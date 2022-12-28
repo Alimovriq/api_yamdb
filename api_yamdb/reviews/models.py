d6dd3cd1d9f105dcba9d686df3e957f9b0beda15
Алимов Ринат <r-alimov@ya.ru>
2022-12-28 15:57:24 +0300

    Модель произведений Title

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Title(models.Model):
+    """Модель произведение."""
+
+    name = models.CharField(max_length=256)
+    year = models.IntegerField()
+    description = models.TextField(null=True)
+
+    class Meta:
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+
+    def __str__(self):
+        return self.name
