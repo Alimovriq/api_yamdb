90eb0af99c93a9044664265cc3abed7ca0271cb9
Алимов Ринат <r-alimov@ya.ru>
2022-12-28 16:33:16 +0300

    Сериалайзер Title

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,20 @@
+import datetime as dt
+
+from rest_framework import serializers
+
+
+from reviews.models import Title
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    """Сериалайзер для произведений."""
+
+    class Meta:
+        model = Title
+        fields = '__all__'
+
+    def validate_year(self, value):
+        current_year = dt.date.today().year()
+        if value > current_year:
+            raise serializers.ValidationError('Проверьте год произведения!')
+        return value
