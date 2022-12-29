25c03e574cacf70206ca78394da0485e0cb2ed3c
Алимов Ринат <r-alimov@ya.ru>
2022-12-30 01:34:49 +0300

    Category Модельки, сериалайзер, эндпойнты, пагинация

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -3,6 +3,27 @@ from django.db import models
 
 User = get_user_model()
 
+
+class Category(models.Model):
+    """Модель категория."""
+
+    name = models.CharField(
+        'Название',
+        max_length=256
+    )
+    slug = models.SlugField(
+        unique=True,
+        max_length=50
+    )
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
 # Написать модельки Жанров и Категорий, привязать к Произведениям,
 # учесть их связь и удаление.
 class Title(models.Model):
@@ -19,6 +40,13 @@ class Title(models.Model):
         'Описание',
         null=True
     )
+    category = models.ForeignKey(
+        Category,
+        null=True,
+        on_delete=models.SET_NULL,
+        verbose_name='Категория',
+        related_name='titles'
+    )
 
     class Meta:
         verbose_name = 'Произведение'
@@ -28,22 +56,22 @@ class Title(models.Model):
         return self.name
 
 
-class Review(models.Model):
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='comments')
-    title = models.ForeignKey(
-        Title, on_delete=models.CASCADE, related_name='comments')
-    text = models.TextField()
-    pub_date = models.DateTimeField(
-        'Дата добавления', auto_now_add=True, db_index=True)
-    score = models.IntegerField()
+# class Review(models.Model):
+#     author = models.ForeignKey(
+#         User, on_delete=models.CASCADE, related_name='comments')
+#     title = models.ForeignKey(
+#         Title, on_delete=models.CASCADE, related_name='comments')
+#     text = models.TextField()
+#     pub_date = models.DateTimeField(
+#         'Дата добавления', auto_now_add=True, db_index=True)
+#     score = models.IntegerField()
 
 
-class Comment(models.Model):
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='comments')
-    review = models.ForeignKey(
-        Review, on_delete=models.CASCADE, related_name='comments')
-    text = models.TextField()
-    pub_date = models.DateTimeField(
-        'Дата добавления', auto_now_add=True, db_index=True)
+# class Comment(models.Model):
+#     author = models.ForeignKey(
+#         User, on_delete=models.CASCADE, related_name='comments')
+#     review = models.ForeignKey(
+#         Review, on_delete=models.CASCADE, related_name='comments')
+#     text = models.TextField()
+#     pub_date = models.DateTimeField(
+#         'Дата добавления', auto_now_add=True, db_index=True)
