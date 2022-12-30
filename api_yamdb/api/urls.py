5833dcee4a6217d8cc6d71c9f7c2049b74fed80b
Alimov Rinat <111974819+Alimovriq@users.noreply.github.com>
2022-12-30 23:40:17 +0300

    Update urls.py

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from api.views import TitleViewSet, CategoryViewSet
+from api.views import TitleViewSet, CategoryViewSet, ReviewViewSet
 
 router_v1 = DefaultRouter()
 
 router_v1.register('titles', TitleViewSet)
 router_v1.register('categories', CategoryViewSet)
+router_v1.register(r'titles/(?P<title_id>\d+)/reviews',
+                   ReviewViewSet, basename='reviews') 
 
 urlpatterns = [
     path('', include(router_v1.urls)),
