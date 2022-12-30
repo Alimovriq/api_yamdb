ca852a2371637f6c732bd2cd27df781e510fb5c1
Дмитрий Щеколов <dschekolov@mail.ru>
2022-12-30 09:10:55 +0300

    Исправление модели Review

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -30,9 +30,9 @@ class Title(models.Model):
 
 class Review(models.Model):
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='comments')
+        User, on_delete=models.CASCADE, related_name='reviews')
     title = models.ForeignKey(
-        Title, on_delete=models.CASCADE, related_name='comments')
+        Title, on_delete=models.CASCADE, related_name='reviews')
     text = models.TextField()
     pub_date = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True)
