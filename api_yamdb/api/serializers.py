98f22d2f8be47efdf67d77e512033737c92e7bc2
Алимов Ринат <r-alimov@ya.ru>
2022-12-31 16:08:46 +0300

    Подправил сериалайзеры жанров, произведений.

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -7,26 +7,12 @@ from rest_framework.validators import UniqueTogetherValidator
 from reviews.models import Title, Category, Genre, Review
 
 
-class TitleSerializer(serializers.ModelSerializer):
-    """Сериалайзер для произведений."""
-
-    class Meta:
-        model = Title
-        fields = '__all__'
-
-    def validate_year(self, value):
-        current_year = dt.date.today().year()
-        if value > current_year:
-            raise serializers.ValidationError('Проверьте год произведения!')
-        return value
-
-
 class CategorySerializer(serializers.ModelSerializer):
     """Сериалайзер для категорий."""
 
     class Meta:
         model = Category
-        fields = '__all__'
+        fields = 'name', 'slug'
 
 
 class GenreSerializer(serializers.ModelSerializer):
@@ -34,7 +20,23 @@ class GenreSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Genre
-        fields = '__all__'
+        fields = 'name', 'slug'
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    """Сериалайзер для произведений."""
+    category = CategorySerializer()
+    genre = GenreSerializer(many=True)
+
+    class Meta:
+        model = Title
+        fields = 'id', 'name', 'year', 'description', 'genre', 'category'
+
+    def validate_year(self, value):
+        current_year = dt.date.today().year()
+        if value > current_year:
+            raise serializers.ValidationError('Проверьте год произведения!')
+        return value
 
 
 class ReviewSerializer(serializers.ModelSerializer):
